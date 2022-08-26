88bbc14f154c05b9808191fa9fe70169ce329ff4
Miguel-leiger <110341302+Miguel-leiger@users.noreply.github.com>
2022-08-26 16:16:16 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { dirname } = require('path');
+
+const path = require('path');
+
+const app = express();
+
+const publichPath = path.resolve(__dirname, './public');
+ app.use ( express.static(publichPath) );
+
+
+app.get('/', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'))});
+
+app.listen(3002, () => console.log('servidor andando'));
