9310aadd2615eed6cf65c6ecafc0008cc5d8983d
Miguel-leiger <migue.lostilos@gmail.com>
2022-09-01 00:45:58 +0200

    nuevos cambios

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const { dirname } = require('path');
-
 const path = require('path');
 
 const app = express();
@@ -12,4 +10,12 @@ const publichPath = path.resolve(__dirname, './public');
 app.get('/', (req,res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))});
 
-app.listen(process.env.PORT || 3002, () => console.log('servidor andando'));
+    app.get('/login', (req,res) => {
+        res.sendFile(path.resolve(__dirname, './views/login.html'))});
+
+app.get('/register', (req,res) => {
+            res.sendFile(path.resolve(__dirname, './views/register.html'))});
+
+    app.listen(process.env.PORT || 3000, () => {
+        console.log("Servidor corriendo")
+    });
